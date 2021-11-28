2172c3addb351c8406b33cc40276e4678e24fa44
John Rosario <john.rosario.francisco@sap.com>
2021-11-28 15:43:06 +0100

    third requirement ready

--- src/Greet.jsx
+++ src/Greet.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Greet = (name) => {
+  if (!name) {
+    return "Hello, my friend";
+  }
+  if (name === name.toUpperCase()) {
+    return `Hello ${name}`.toUpperCase();
+  }
   return `Hello, ${name}`;
 };
 
