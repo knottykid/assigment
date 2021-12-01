277b777189967b54b37f67bb84c1125cc75965af
John Rosario <john.rosario.francisco@sap.com>
2021-12-01 11:57:03 +0100

    more descriptive test

--- src/Greet.test.js
+++ src/Greet.test.js
@@ -2,17 +2,17 @@ import Greet from "./Greet";
 // import {expect} from 'jest'
 
 describe("Greet", () => {
-  it("works", () => {
+  it("Hello, name", () => {
     const expected = "Hello, name";
     const result = Greet("name");
     expect(result).toBe(expected);
   });
-  it("works", () => {
+  it("Hello, my friend", () => {
     const expected = "Hello, my friend";
     const result = Greet(null);
     expect(result).toBe(expected);
   });
-  it("works", () => {
+  it("HELLO NAME", () => {
     const expected = "HELLO NAME";
     const result = Greet("name".toUpperCase());
     expect(result).toBe(expected);
